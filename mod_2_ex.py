e0e5ece8ec26fa7e44819c2f833a8d10c21f1424
notmasonbrooks <masonbrooksentertainment@gmail.com>
2024-04-26 19:26:37 -0700

    Mod 2 Excersise

---
+++ mod_2_ex.py
@@ -0,0 +1,45 @@
+full_name = "Mason Brooks"
+
+print(full_name)
+
+myName = "Mason"
+myname = "Brooks"
+
+print(myName)
+print(myname)
+
+fav_color = "Green"
+
+print(fav_color)
+
+fruit1, fruit2, fruit3 = "apple", "banana", "cherry"
+
+print(fruit1, fruit2, fruit3)
+print(fruit1)
+print(fruit2)
+print(fruit3)
+
+int_var = 21
+float_var = 2.21
+str_var = "Hi Mom"
+
+print(int_var, float_var, str_var)
+print(type(str_var))
+print(type(int_var))
+print(type(float_var))
+
+mystery_var = "1234"
+
+var_type = (type(mystery_var))
+
+print(var_type)
+
+dynamic_var = 10
+print(dynamic_var, type(dynamic_var))
+
+dynamic_var = "Hi Mom"
+print(dynamic_var, type(dynamic_var))
+
+    
+PI = 3.14159
+print(type(PI))
---
+++ python_multi-line_test.py
@@ -0,0 +1,13 @@
+a = '''My favorite hobbies currently are golf and photography
+Sometimes the two go hand-in-hand
+In the mornings when the fog rolls over the hills, is the perfect time for some landscape photos and a good tee shot.'''
+
+print (a)
+
+#the equals sign needs to be doubled for it to register for an equation, dont forget the colon at the end.
+
+b = 5
+c = 7
+
+if b + c == 12: 
+    print ("Booyah!")
